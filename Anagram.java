276c5a7ca511a96e07dd505df84fde10714baff0
Gopika55 <88382739+Gopika55@users.noreply.github.com>
2024-03-01 16:42:29 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,45 @@
+package gopika;
+
+public class Anagram {
+
+	public static void main(String args[])
+	{
+		String s="anagram";
+		String t="nagaram";
+		System.out.println("Is Anagram"+isanagram(s,t));
+	}
+
+	public static boolean isanagram(String s, String t) {
+		
+		int m=s.length();
+		int n=t.length();
+		
+		if(m!=n)
+		{
+			return false;
+		}
+		
+		else
+		{
+			int count[]=new int[26];
+			
+			for(int i=0;i<m;i++)
+			{
+				count[s.charAt(i)-'a']++;
+			}
+			for(int i=0;i<n;i++)
+			{
+				count[t.charAt(i)-'a']--;
+			}
+			
+			for(int i=0;i<count.length;i++)
+			{
+				if(count[i]!=0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
